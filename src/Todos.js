13eb2aef57ddf1bbabd5a78bac9afc707b0bf439
Runkun <miaorunkun@163.com>
2020-11-22 21:54:15 +0800

    fonts htmlFor encode html code

--- src/Todos.js
+++ src/Todos.js
@@ -1,4 +1,5 @@
 import react, {Component, Fragment} from 'react';
+import './Todos.css'
 
 class Todos extends Component{
   constructor(props){
@@ -15,18 +16,26 @@ class Todos extends Component{
     return(
       <Fragment>
         <header>
+          <label htmlFor="insertArea">输入内容</label>
+          {/*以下是输入框，输入todos*/}
           <input
+            id = "insertArea"
+            className='input'
             value={this.state.inputValue}
             onChange={this.handleInput.bind(this)}
           />
+          {/*以下是提交键，提交todos*/}
           <button onClick={this.handleButtonClick.bind(this) }>提交</button>
         </header>
         <div>
+          {/*以下是显示，显示todos*/}
           {
             this.state.list.map(
               (item, idx) =>
-              <li key={idx} onClick={this.deleteItem.bind(this, idx)}>
-              {item}
+              <li
+              key={idx}
+              onClick={this.deleteItem.bind(this, idx)}
+              dangerouslySetInnerHTML={{__html: item}}>
               </li>
             )
           }
@@ -57,7 +66,7 @@ class Todos extends Component{
     todoLis.splice(idx, 1)
     this.setState(
       {
-        list: [todoLis]
+        list: todoLis
       }
     )
   }
